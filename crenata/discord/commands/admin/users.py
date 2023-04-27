09a235ee8d0fcc7436895c04c49c8f84bafd400a
Starcea <stardev.uwu@gmail.com>
2023-04-27 14:34:11 +0900

    fix: newline and count

--- crenata/discord/commands/admin/users.py
+++ crenata/discord/commands/admin/users.py
@@ -7,6 +7,8 @@ from discord import Embed
 async def users(interaction: CrenataInteraction) -> None:
     users = await interaction.client.ctx.query.user.read_all()
 
+    # users += [interaction.user for _ in range(323)]  # for testing
+
     embed = Embed(
         title="유저 수", description=f"현재 {len(users)}명의 유저가 봇을 사용하고 있어요.", color=5681003
     )
@@ -14,18 +16,19 @@ async def users(interaction: CrenataInteraction) -> None:
     if len(users) < 10:
         embed.add_field(
             name="유저 목록",
-            value=" ".join([f"<@{user.id}>" for user in users]),
+            value="\n".join([f"<@{user.id}>" for user in users]),
         )
 
         return await interaction.response.send_message(
             embed=embed,
         )
 
+    nl = "\n"
     embed.add_field(
         name="유저 목록",
         value=(
-            f"{' '.join([f'<@{user.id}>' for user in users[:9]])} and "
-            f"{len(users) - 10} more"
+            f"{nl.join([f'<@{user.id}>' for user in users[:10]])}\n ...and"
+            f" {len(users) - 10} more"
         ),
     )
 
