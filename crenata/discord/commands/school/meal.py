114f2069f087fcd7844cb5f2522b8277eadea101
최민건 <2019043090@hanyang.ac.kr>
2023-05-19 20:17:37 +0900

    feat: 급식 알러지 정보 추가

--- crenata/discord/commands/school/meal.py
+++ crenata/discord/commands/school/meal.py
@@ -3,10 +3,11 @@ from typing import Literal, Optional
 
 from crenata.discord import CrenataInteraction
 from crenata.discord.commands.school import school
-from crenata.discord.embed import allergy_page, meal_page
+from crenata.discord.embed import meal_page
 from crenata.discord.interaction import school_info
 from crenata.utils.discord import ToDatetime, dynamic_send
 from discord import app_commands, ui
+from discord.interactions import Interaction
 
 
 @school.command(name="급식", description="급식 식단표를 가져와요.")
@@ -41,30 +42,23 @@ async def meal(
         )
         return
 
-    """async def allergy_callback(interaction):
-        await interaction.response.send_message(
-            embed=allergy_page(),
-            ephemeral=True,
-        )"""
     user = interaction.user.id
-    async def callback_no_response(interaction):
+    selAllergy: ui.Select[ui.View] = ui.Select(placeholder="알러지 정보")
+
+    async def callback_no_response(interaction: Interaction) -> None:
         if interaction.user.id == user:
             selAllergy.placeholder = selAllergy.values[0]
         await interaction.response.edit_message(view=view)
 
     view = ui.View()
 
-    lst = []
-    selAllergy = ui.Select(placeholder="알러지 정보")
     selAllergy.add_option(label="1.난류, 2.우유, 3.메밀")
     selAllergy.add_option(label="4.땅콩, 5.대두, 6.밀")
     selAllergy.add_option(label="7.고등어, 8.게, 9.새우")
     selAllergy.add_option(label="10.돼지고기, 11.복숭아, 12.토마토")
     selAllergy.add_option(label="13.아황산염, 14.호두, 15.닭고기")
     selAllergy.add_option(label="16.쇠고기, 17.오징어, 18.조개류")
-    selAllergy.callback = callback_no_response
-    # btnAllergy = ui.Button(label="알러지 내역 확인")
-    # btnAllergy.callback = allergy_callback
+    setattr(selAllergy, "callback", callback_no_response)
     view.add_item(selAllergy)
 
     await dyn(embed=meal_info, ephemeral=preferences.ephemeral, view=view, content=None)
