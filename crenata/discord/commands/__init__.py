772f17bfac73a4d836f862918c6ce56066b9e4df
Starcea <stardev.uwu@gmail.com>
2023-06-13 16:53:33 +0900

    feat: path_split util

--- crenata/discord/commands/__init__.py
+++ crenata/discord/commands/__init__.py
@@ -1,8 +1,16 @@
 from glob import glob
 from importlib import import_module
+from os.path import split, splitext
 from typing import Any
 
 from discord.app_commands import Command, Group
+
+
+def path_split(path: str) -> list[str]:
+    head, tail = split(path)
+    if not head:
+        return [tail]
+    return path_split(head) + [splitext(tail)[0]]
 
 
 def load_commands(path: str) -> list[Command[Any, ..., Any] | Group]:
@@ -13,7 +21,7 @@ def load_commands(path: str) -> list[Command[Any, ..., Any] | Group]:
         if file.endswith("__init__.py"):
             continue
 
-        module = import_module(file.replace("/", ".").replace("\\", ".")[:-3])
+        module = import_module(".".join(path_split(file)))
 
         for name in dir(module):
             attr = getattr(module, name)
