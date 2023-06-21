61469ee1513e6e51cf50b1d13d235346c8c00f28
Ryu Juheon <saidbysolo@gmail.com>
2023-06-21 14:19:20 +0000

    feat: more scalable

--- crenata/discord/paginator.py
+++ crenata/discord/paginator.py
@@ -1,14 +1,15 @@
 from functools import cached_property
 from typing import Any, Optional
 
+from crenata.discord.selecter import Selector
+from crenata.discord.view import CrenataView
 from crenata.utils.discord import CrenataEmbed
 from discord.enums import ButtonStyle
 from discord.interactions import Interaction
 from discord.ui.button import button
-from discord.ui.view import View
 
 
-class Paginator(View):
+class Paginator(CrenataView):
     """
     페이지를 넘길수있는 상호작용입니다.
     """
@@ -19,26 +20,13 @@ class Paginator(View):
         embeds: list[CrenataEmbed],
         timeout: Optional[float] = 60,
     ):
-        super().__init__(timeout=timeout)
+        super().__init__(timeout=timeout, executor_id=executor_id)
         self.embeds = embeds
-        self.executor_id = executor_id
         self.index = 0
-        self.selected = False
 
     @cached_property
     def total(self) -> int:
         return len(self.embeds)
-
-    async def interaction_check(self, interaction: Interaction) -> bool:
-        if user := interaction.user:
-            if user.id == self.executor_id:
-                return True
-
-            await interaction.response.send_message(
-                "명령어 실행자만 상호작용이 가능합니다.", ephemeral=True
-            )
-
-        return False
 
     @button(label="이전", style=ButtonStyle.primary, emoji="◀")
     async def prev(self, interaction: Interaction, _: Any) -> None:
@@ -58,13 +46,6 @@ class Paginator(View):
 
         await interaction.response.edit_message(embed=self.embeds[self.index])
 
-    @button(label="확인", style=ButtonStyle.success, emoji="✅")
-    async def ok(self, interaction: Interaction, _: Any) -> None:
-        self.selected = True
-        await interaction.response.defer()
-        self.stop()
 
-    @button(label="닫기", style=ButtonStyle.danger, emoji="✖️")
-    async def close(self, interaction: Interaction, _: Any) -> None:
-        await interaction.response.defer()
-        self.stop()
+class SelectablePaginator(Paginator, Selector):
+    ...
