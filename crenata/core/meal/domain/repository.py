9d23f9a5b00fc7e18cea98769ee6698d62b3ccac
Ryu Juheon <saidbysolo@gmail.com>
2023-08-29 13:16:03 +0000

    feat: 인수 변경

--- crenata/core/meal/domain/repository.py
+++ crenata/core/meal/domain/repository.py
@@ -1,10 +1,11 @@
 from abc import ABC
+from datetime import datetime
 
 from crenata.core.meal.domain.entity import Meal
-from crenata.core.school.domain.entity import School
-from crenata.core.schoolinfo.domain.entity import SchoolInfo
 
 
 class MealRepository(ABC):
-    async def get_meal(self, school: School | SchoolInfo) -> list[Meal]:
+    async def get_meal(
+        self, edu_office_code: str, standard_school_code: str, date: datetime
+    ) -> list[Meal]:
         ...
--- crenata/infrastructure/neispy/meal/domain/repository.py
+++ crenata/infrastructure/neispy/meal/domain/repository.py
@@ -1,20 +1,23 @@
+from datetime import datetime
+
 from neispy.client import Neispy
 
 from crenata.core.meal.domain.entity import Meal
 from crenata.core.meal.domain.repository import MealRepository
-from crenata.core.school.domain.entity import School
-from crenata.core.schoolinfo.domain.entity import SchoolInfo
-from crenata.infrastructure.utils.datetime import to_datetime
+from crenata.infrastructure.utils.datetime import to_datetime, to_yyyymmdd
 
 
 class MealRepositoryImpl(MealRepository):
     def __init__(self, neispy: Neispy) -> None:
         self.neispy = neispy
 
-    async def get_meal(self, school: School | SchoolInfo) -> list[Meal]:
+    async def get_meal(
+        self, edu_office_code: str, standard_school_code: str, date: datetime
+    ) -> list[Meal]:
         r = await self.neispy.mealServiceDietInfo(
-            ATPT_OFCDC_SC_CODE=school.edu_office_code,
-            SD_SCHUL_CODE=school.standard_school_code,
+            ATPT_OFCDC_SC_CODE=edu_office_code,
+            SD_SCHUL_CODE=standard_school_code,
+            MLSV_YMD=to_yyyymmdd(date),
         )
         row = r.mealServiceDietInfo[1].row
 
