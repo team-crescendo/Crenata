900f3beee0019bc0c9dca54d2ef51e58bac70585
Starcea <stardev.uwu@gmail.com>
2023-10-02 05:17:44 +0900

    fix: fix string typos

--- crenata/core/strings.py
+++ crenata/core/strings.py
@@ -1,16 +1,12 @@
 class Strings:
     USER_NOT_FOUND = "가입되지 않은 사용자입니다."
     DUPLICATE_USER = "이미 가입된 사용자입니다."
-    MEAL_NAME_NOT_FOUND = (
-        "해당하는 식사명을 찾을수 없습니다. 조식, 중식, 석식중에 다시 선택해주시길 바랍니다."
-    )
+    MEAL_NAME_NOT_FOUND = "해당하는 식사 명을 찾을 수 없습니다. 조식, 중식, 석식 중에 다시 선택해 주시길 바랍니다."
 
     USER_CANCELLED = "취소했습니다."
     VIEW_TIMEOUT = "시간 초과되었습니다."
-    NEED_SCHOOL_REGISTER = "학교를 먼저 등록해주세요."
+    NEED_SCHOOL_REGISTER = "학교를 먼저 등록해 주세요."
     DATE_PARSE_ERROR = (
-        "날짜를 잘못 입력했습니다. "
-        "YYYYMMDD 형식, 또는 '내일'로 입력해주시길 바랍니다.. "
-        "예: 20220110, 내일"
+        "날짜를 잘못 입력했습니다. YYYYMMDD 형식, 또는 '내일'로 입력해 주시길 바랍니다.. 예: 20220110, 내일"
     )
-    INTERACTION_LOCKED = "다른 명령어을 사용중일때는 해당 명령어를 사용할수없습니다."
+    INTERACTION_LOCKED = "다른 명령어을 사용 중일 때는 해당 명령어를 사용할 수 없습니다."
