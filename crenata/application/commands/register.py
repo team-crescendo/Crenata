b55d80da9b723f36f48791667eaa9911e824e34c
Starcea <stardev.uwu@gmail.com>
2023-10-02 05:21:20 +0900

    feat: register string

--- crenata/application/commands/register.py
+++ crenata/application/commands/register.py
@@ -1,9 +1,9 @@
-from discord import Embed, Interaction, app_commands
-
 from crenata.application.client import Crenata
+from crenata.core.strings import Strings
 from crenata.core.user.domain.entity import User
 from crenata.core.user.usecases.create import CreateUserUseCase
 from crenata.infrastructure.sqlalchemy.user.domain.repository import UserRepositoryImpl
+from discord import Embed, Interaction, app_commands
 
 
 @app_commands.command(name="가입", description="가입합니다.")
@@ -12,6 +12,6 @@ async def register(interaction: Interaction[Crenata]) -> None:
     usecase = CreateUserUseCase(repository)
     await usecase.execute(User.default(interaction.user.id))
 
-    embed = Embed(title="가입 완료", description="가입이 완료되었습니다.")
+    embed = Embed(title="가입 완료", description=Strings.REGISTER_COMPLETED)
 
     await interaction.response.send_message(embed=embed, ephemeral=True)
--- crenata/core/strings.py
+++ crenata/core/strings.py
@@ -1,9 +1,11 @@
 class Strings:
+    REGISTER_COMPLETED = "가입이 완료되었습니다."
+
     USER_NOT_FOUND = "가입되지 않은 사용자입니다."
     DUPLICATE_USER = "이미 가입된 사용자입니다."
     MEAL_NAME_NOT_FOUND = "해당하는 식사 명을 찾을 수 없습니다. 조식, 중식, 석식 중에 다시 선택해 주시길 바랍니다."
 
-    USER_CANCELLED = "취소했습니다."
+    USER_CANCELLED = "취소되었습니다."
     VIEW_TIMEOUT = "시간 초과되었습니다."
     NEED_SCHOOL_REGISTER = "학교를 먼저 등록해 주세요."
     DATE_PARSE_ERROR = (
