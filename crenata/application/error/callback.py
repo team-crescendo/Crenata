415815d7b835ff526954fdda344336e438a7df46
Starcea <stardev.uwu@gmail.com>
2023-11-12 12:17:59 +0900

    style: apply black

--- crenata/application/error/callback.py
+++ crenata/application/error/callback.py
@@ -21,9 +21,12 @@ async def confirmed(
     await interaction.response.send_message(error.message)
 
 
-@error_handler.handle_this_exception(MealNameNotFound | UserNotFound | TimetableNotFound)
+@error_handler.handle_this_exception(
+    MealNameNotFound | UserNotFound | TimetableNotFound
+)
 async def superposition(
-    interaction: Interaction[Crenata], error: MealNameNotFound | UserNotFound | TimetableNotFound
+    interaction: Interaction[Crenata],
+    error: MealNameNotFound | UserNotFound | TimetableNotFound,
 ) -> None:
     if interaction.response.is_done():
         await interaction.edit_original_response(
