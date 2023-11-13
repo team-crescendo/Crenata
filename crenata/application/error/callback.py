f385828caf6b31e1e3b5791b853654597086c39b
Starcea <stardev.uwu@gmail.com>
2023-11-13 20:11:33 +0900

    fix: address unhandled exceptions

--- crenata/application/error/callback.py
+++ crenata/application/error/callback.py
@@ -1,6 +1,15 @@
 from discord import Interaction
 
 from crenata.application.client import Crenata
+from crenata.application.error.exceptions import (
+    DateParseError,
+    InteractionLocked,
+    MustBeGreaterThanZero,
+    NeedGradeAndRoomArgument,
+    NotInteractedUser,
+    UserCancelled,
+    ViewTimeout,
+)
 from crenata.application.error.handler import ErrorHandler
 from crenata.core.meal.exceptions import MealNameNotFound
 from crenata.core.schoolinfo.exceptions import SchoolInfoNotFound
@@ -12,19 +21,38 @@ error_handler: ErrorHandler[Crenata] = ErrorHandler()
 
 @error_handler.handle_this_exception(
     DuplicateUser,
+    NotInteractedUser,
     SchoolInfoNotFound,
 )
 async def confirmed(
     interaction: Interaction[Crenata],
-    error: DuplicateUser | SchoolInfoNotFound,
+    error: DuplicateUser | SchoolInfoNotFound | NotInteractedUser,
 ) -> None:
     await interaction.response.send_message(error.message)
 
 
-@error_handler.handle_this_exception(MealNameNotFound, UserNotFound, TimetableNotFound)
+@error_handler.handle_this_exception(
+    MealNameNotFound,
+    UserNotFound,
+    TimetableNotFound,
+    DateParseError,
+    InteractionLocked,
+    MustBeGreaterThanZero,
+    NeedGradeAndRoomArgument,
+    UserCancelled,
+    ViewTimeout,
+)
 async def superposition(
     interaction: Interaction[Crenata],
-    error: MealNameNotFound | UserNotFound | TimetableNotFound,
+    error: MealNameNotFound
+    | UserNotFound
+    | TimetableNotFound
+    | DateParseError
+    | InteractionLocked
+    | MustBeGreaterThanZero
+    | NeedGradeAndRoomArgument
+    | UserCancelled
+    | ViewTimeout,
 ) -> None:
     if interaction.response.is_done():
         await interaction.edit_original_response(
