3fba4537f66569c80fe18d249dceb4a9c165d85d
Starcea <stardev.uwu@gmail.com>
2023-11-24 20:16:55 +0900

    refactor(profile): refactor codes

--- crenata/application/commands/profile.py
+++ crenata/application/commands/profile.py
@@ -1,34 +1,39 @@
 from discord import Interaction, app_commands
 
 from crenata.application.client import Crenata
-from crenata.core.user.usecases.get import GetUserUseCase
-from crenata.infrastructure.sqlalchemy.user.domain.repository import UserRepositoryImpl
 from crenata.application.embeds.profile import profile_embed_builder
+from crenata.core.schoolinfo.usecases.get import GetSchoolInfoUseCase
+from crenata.core.user.usecases.get import GetUserUseCase
+from crenata.infrastructure.sqlalchemy.schoolinfo.domain.repository import (
+    SchoolInfoRepositoryImpl,
+)
+from crenata.infrastructure.sqlalchemy.user.domain.repository import UserRepositoryImpl
 
 
 @app_commands.command(name="프로필", description="내 프로필을 확인합니다.")
 async def profile(interaction: Interaction[Crenata]) -> None:
-    repository = UserRepositoryImpl(interaction.client.database)
-    user = await GetUserUseCase(repository).execute(interaction.user.id)
+    user_repository = UserRepositoryImpl(interaction.client.database)
+    schoolinfo_repository = SchoolInfoRepositoryImpl(interaction.client.database)
 
-    user_name = interaction.user.global_name
-    if user_name == None:
-        user_name = ""
-
-    school_name = ""
-    school_grade = ""
-    school_room = ""
+    user = await GetUserUseCase(user_repository).execute(interaction.user.id)
+    school_info = await GetSchoolInfoUseCase(schoolinfo_repository).execute(
+        interaction.user.id
+    )
 
     is_private = user.preferences.private
     is_empheral = user.preferences.ephemeral
 
-    if user.school_info != None:
-        school_name = user.school_info.name
-        school_grade = str(user.school_info.grade)
-        school_room = str(user.school_info.room)
+    school_name = school_info.name
+    school_grade = str(school_info.grade)
+    school_room = str(school_info.room)
 
     embed = profile_embed_builder(
-        user_name, school_name, school_grade, school_room, is_private, is_empheral
+        interaction.user,
+        school_name,
+        school_grade,
+        school_room,
+        is_private,
+        is_empheral,
     )
 
     await interaction.response.send_message(embed=embed, ephemeral=is_empheral)
--- crenata/application/embeds/profile.py
+++ crenata/application/embeds/profile.py
@@ -1,65 +1,50 @@
+from discord import Member, User
+
 from crenata.application.embeds import CrenataEmbed
 from crenata.application.utils import follow_private_preference
 
 
 def profile_embed_builder(
-    user_name: str,
+    user: User | Member,
     school_name: str,
     school_grade: str,
     school_room: str,
     private: bool,
     ephemeral: bool,
 ) -> CrenataEmbed:
-    embed = CrenataEmbed()
+    embed = CrenataEmbed(title=user.name)
 
-    embed.title = "프로필"
-    embed.add_field(name=user_name, value="", inline=False)
+    embed.set_thumbnail(url=user.display_avatar.url)
+    embed.set_author(name="프로필")
 
-    if school_name != "":
-        grade_room = school_grade + "학년 " + school_room + "반"
+    grade_room = f"{school_grade}학년 {school_room}반"
 
-        (
-            school_name,
-            grade_room,
-        ) = follow_private_preference(
-            is_private=private, school_name=school_name, grade_room=grade_room
-        ).values()
+    (
+        school_name,
+        grade_room,
+    ) = follow_private_preference(
+        is_private=private, school_name=school_name, grade_room=grade_room
+    ).values()
 
-        embed.add_field(
-            name="🏫 내 학교 정보",
-            value="**[학교]** " + school_name + "\n**[학년/반]** " + grade_room + "\n",
-            inline=False,
-        )
-        embed.add_field(
-            name="", value="> `/학교 설정`으로 학교 정보 변경이 가능합니다." + "\n", inline=False
-        )
-        embed.add_field(name="", value="-----", inline=False)
-
-    if private:
-        txt_private = "❌ 비공개"
-    else:
-        txt_private = "⭕ 공개"
-
-    if ephemeral:
-        txt_ephemeral = "❌ 비공개"
-    else:
-        txt_ephemeral = "⭕ 공개"
+    embed.add_field(
+        name="🏫 내 학교 정보",
+        value=(
+            f"**[학교]** {school_name}\n**[학년/반]** {grade_room}\n\n"
+            "> `/학교 설정`으로 학교 정보 변경이 가능합니다.\n\n"
+            "-----"
+        ),
+        inline=False,
+    )
 
     embed.add_field(
         name="🔒 공개 여부 설정",
-        value="**[내 학교 공개하기]** "
-        + txt_private
-        + "\n**[명령어 답변 공개하기]** "
-        + txt_ephemeral
-        + "\n",
+        value=(
+            f"**[내 학교 공개하기]** {'❌ 비공개' if private else '⭕ 공개'}\n"
+            f"**[명령어 답변 공개하기]** {'❌ 비공개' if ephemeral else '⭕ 공개'}\n\n"
+            "> `/환경설정 변경`으로 환경설정 변경이 가능합니다.\n\n"
+            "-----"
+        ),
         inline=False,
     )
-    embed.add_field(
-        name="", value="> `/환경설정 변경`으로 환경설정 변경이 가능합니다." + "\n", inline=False
-    )
-    embed.add_field(name="", value="-----", inline=False)
-
-    # embed.description = f"{school_name} __{datetime_to_readable(date)}__ 시간표"
-    # embed.set_image(url="attachment://timetable.png")
 
     return embed
