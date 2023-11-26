1f36912a9a8626dd7d144aa6ecb672ad9191b70c
Ryu Juheon <saidbysolo@gmail.com>
2023-11-26 15:36:38 +0000

    feat: embed 분리

--- crenata/application/commands/exit.py
+++ crenata/application/commands/exit.py
@@ -1,12 +1,12 @@
-from discord import Embed, Interaction, app_commands
-
 from crenata.application.client import Crenata
+from crenata.application.embeds.exit import exit_embed_builder
 from crenata.application.utils import InteractionLock
 from crenata.application.view.confirm import Confirm
 from crenata.core.strings import Strings
 from crenata.core.user.usecases.delete import DeleteUserUseCase
 from crenata.core.user.usecases.get import GetUserUseCase
 from crenata.infrastructure.sqlalchemy.user.domain.repository import UserRepositoryImpl
+from discord import Interaction, app_commands
 
 
 @app_commands.command(name="탈퇴", description="탈퇴합니다.")
@@ -16,11 +16,7 @@ async def exit(interaction: Interaction[Crenata]) -> None:
 
         user = await GetUserUseCase(user_repository).execute(interaction.user.id)
 
-        embed = Embed(
-            title="탈퇴",
-            description="정말 탈퇴하시겠습니까?",
-            color=5681003,
-        )
+        embed = exit_embed_builder()
 
         view = Confirm(interaction.user.id)
         await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
