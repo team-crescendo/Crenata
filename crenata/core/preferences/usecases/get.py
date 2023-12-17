2f27ea507f0ee85362235932965463899b2deaa4
Ryu Juheon <saidbysolo@gmail.com>
2023-12-17 14:46:48 +0000

    feat: more safety reference preferences

--- crenata/core/preferences/usecases/get.py
+++ crenata/core/preferences/usecases/get.py
@@ -3,14 +3,14 @@ from crenata.core.preferences.domain.repository import PreferencesRepository
 from crenata.core.preferences.exceptions import PreferencesNotFound
 
 
-class UpdatePreferencesUseCase:
+class GetPreferencesUseCase:
     def __init__(self, preferences_repository: PreferencesRepository) -> None:
         self.preferences_repository = preferences_repository
 
-    async def execute(self, user_id: int, preferences: Preferences) -> None:
+    async def execute(self, user_id: int) -> Preferences:
         nullable_prefernces = await self.preferences_repository.get_preferences(user_id)
 
         if nullable_prefernces is None:
             raise PreferencesNotFound
 
-        await self.preferences_repository.update_preferences(user_id, preferences)
+        return nullable_prefernces
--- crenata/core/strings.py
+++ crenata/core/strings.py
@@ -11,6 +11,12 @@ class CoreStrings:
     SCHOOL_NOT_FOUND = "해당하는 학교를 찾을 수 없습니다."
     USER_NOT_FOUND = "가입되지 않은 사용자입니다."
     DUPLICATE_USER = "이미 가입된 사용자입니다."
-    MEAL_NAME_NOT_FOUND = "해당하는 식사 명을 찾을 수 없습니다. 조식, 중식, 석식 중에 다시 선택해 주시길 바랍니다."
+    MEAL_NAME_NOT_FOUND = (
+        "해당하는 식사 명을 찾을 수 없습니다. 조식, 중식, 석식 중에 다시 선택해 주시길"
+        " 바랍니다."
+    )
+    PREFERENCES_NOT_FOUND = "환경설정을 찾을 수 없습니다."
 
-    SCHOOL_INFO_NOT_FOUND = "학교 정보를 찾을 수 없습니다. 학교를 먼저 등록해 주시기 바랍니다."
+    SCHOOL_INFO_NOT_FOUND = (
+        "학교 정보를 찾을 수 없습니다. 학교를 먼저 등록해 주시기 바랍니다."
+    )
