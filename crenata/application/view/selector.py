aa6acb7f05fbd71665d1f7a16ecbc550ee3e72ae
Ryu Juheon <saidbysolo@gmail.com>
2024-01-08 12:45:12 +0000

    refactor: Button generic type is CrenataView

--- crenata/application/view/selector.py
+++ crenata/application/view/selector.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 from discord import ButtonStyle, Interaction
 from discord.ui import Button, button
 
@@ -8,13 +6,13 @@ from crenata.application.view import CrenataView
 
 class Selector(CrenataView):
     @button(label="확인", style=ButtonStyle.success, emoji="✅")
-    async def confirm(self, interaction: Interaction, _: Button[Any]) -> None:
+    async def confirm(self, interaction: Interaction, _: Button[CrenataView]) -> None:
         self.is_confirm = True
         await interaction.response.defer()
         self.stop()
 
     @button(label="닫기", style=ButtonStyle.danger, emoji="✖️")
-    async def close(self, interaction: Interaction, _: Button[Any]) -> None:
+    async def close(self, interaction: Interaction, _: Button[CrenataView]) -> None:
         self.is_confirm = False
         await interaction.response.defer()
         self.stop()
