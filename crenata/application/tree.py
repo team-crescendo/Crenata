64dd7c5156eb85ac7d90a3a7519306faec7d5b9b
Ryu Juheon <saidbysolo@gmail.com>
2024-02-15 23:16:18 +0900

    feat: use generic type to ClientT (#58)
    * feat: use generic type to ClientT
    
    * style: apply code style

--- crenata/application/tree.py
+++ crenata/application/tree.py
@@ -1,25 +1,25 @@
 from __future__ import annotations
 
 from discord import Interaction
+from discord._types import ClientT
 from discord.app_commands.errors import AppCommandError
 from discord.app_commands.tree import CommandTree
 
-from crenata.application.client import Crenata
 from crenata.application.error.handler import ErrorHandler
 
 
-class CrenataCommandTree(CommandTree[Crenata]):
-    def __init__(self, client: Crenata, *, fallback_to_global: bool = True):
+class CrenataCommandTree(CommandTree[ClientT]):
+    def __init__(self, client: ClientT, *, fallback_to_global: bool = True):
         super().__init__(client, fallback_to_global=fallback_to_global)
-        self.error_handler: ErrorHandler[Crenata] = ErrorHandler()
+        self.error_handler: ErrorHandler[ClientT] = ErrorHandler()
 
-    def set_error_handler(self, handler: ErrorHandler[Crenata]) -> None:
+    def set_error_handler(self, handler: ErrorHandler[ClientT]) -> None:
         self.error_handler = handler
 
     async def on_error(
-        self, interaction: Interaction[Crenata], error: AppCommandError
+        self, interaction: Interaction[ClientT], error: AppCommandError
     ) -> None:
         await self.error_handler.on_error(interaction, error)
 
-    async def interaction_check(self, interaction: Interaction[Crenata]) -> bool:
+    async def interaction_check(self, interaction: Interaction[ClientT]) -> bool:
         ...
